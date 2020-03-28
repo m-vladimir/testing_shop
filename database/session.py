b336d1572bc93a70c0aac64b1c3c74122d746023
Vladimir Marimorich <marimorich.vladimir@gmail.com>
2020-03-28 14:32:32 +0200

    Add basic files to operate database
    Add database/ folder with basic sqlalchemy model to inherit other models
    from it and Session object for further database manipulations.

---
+++ database/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core import config
+
+
+engine = create_engine(config.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
